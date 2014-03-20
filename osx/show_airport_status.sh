d035cfe23eb99c35b2a33c4bc06961b71efe108b
Michael Holzinger <mike.holzinger@gmail.com>
2014-03-19 22:09:39 -0700

    Show airport status

---
+++ osx/show_airport_status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I
+
+# Sniffing on channel 1:
+#sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport en1 sniff 1
+
+# Help
+/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I
