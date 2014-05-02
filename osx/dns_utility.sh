c80e3ec31b8fee42594ce84556c9e5112a938b13
Michael Holzinger <mike.holzinger@gmail.com>
2014-05-02 13:20:27 -0700

    getopt parsing dns util

--- osx/dns_utility.sh
+++ osx/dns_utility.sh
@@ -14,24 +14,29 @@ interface=Wi-Fi
 initializeANSI()
 {
 #  esc="\033" # if this doesn't work, enter an ESC directly
-  esc=""
-
-  blackf="${esc}[30m";   redf="${esc}[31m";    greenf="${esc}[32m"
-  yellowf="${esc}[33m"   bluef="${esc}[34m";   purplef="${esc}[35m"
-  cyanf="${esc}[36m";    whitef="${esc}[37m"
-
-  reset="${esc}[0m"
+    esc=""
+    blackf="${esc}[30m";   
+    redf="${esc}[31m";    
+    greenf="${esc}[32m";
+    yellowf="${esc}[33m";   
+    bluef="${esc}[34m";   
+    purplef="${esc}[35m";
+    cyanf="${esc}[36m";    
+    whitef="${esc}[37m";  
+    reset="${esc}[0m";
 }
 
 # evaluate passed parameters, if none display DNS and exit with help statement
 
+# THIS SCRIPT
+prog=$( echo $0 | sed 's|^\.||' | awk '{gsub(/\/.*\//,"",$1); print}' )
+
 print_usage()
 {
-    echo "Usage : ["$0"] [option]"
-    echo "    g = set Google DNS entries"
-    echo "    o = set OpenDNS entries"
-    echo "    a = set autoconfig with DHCP"
-    echo "    p = print out current DNS setting"
+    echo "usage: "$prog" [-a auto] [-g google] [-h help] [-o opendns] [-p print]"
+    echo "  This utility sets ["$interface"] DNS entries to Google, OpenDNS or DHCP host (auto)"
+    echo "  eg: $prog -g   <--- sets the "$interface" interface to use Google DNS"
+    exit;
 }
 
 print_dns_entry()
@@ -43,25 +48,27 @@ print_dns_entry()
     fi
 
     echo "Current DNS server entries on this Mac :"
-	echo ${yellowf}$dns_value${reset}
-
-	#    scutil --dns | grep nameserver | cut -d : -f 2 | sort -u
+    echo ${yellowf}$dns_value${reset}
+#    echo
+#    echo "Settings in /etc/resolv.conf"
+#    cat /etc/resolv.conf | sed '/#/d'
+#    scutil --dns | grep nameserver | cut -d : -f 2 | sort -u
 }
 
 edit_nameserver_interface()
 {
-	# USING NETWORKSETUP
-	if [ "$1" == "empty" ]; then
-		echo ${yellowf}"DHCP set"${reset}
+    # USING NETWORKSETUP
+    if [ "$1" == "empty" ]; then
+        echo ${yellowf}"DHCP set"${reset}
     else
-	    echo "New entries :" ${yellowf}$1 $2${reset}
+        echo "New entries :" ${yellowf}$1 $2${reset}
     fi
-    sudo networksetup -setdnsservers $interface $1 $2	
+    sudo networksetup -setdnsservers $interface $1 $2    
 }
 
 edit_searchdomain()
 {
-	sudo networksetup -setsearchdomains $interface $1
+    sudo networksetup -setsearchdomains $interface $1
 }
 
 initializeANSI
@@ -71,32 +78,35 @@ if [ "$#" -lt 1 ]; then
     print_usage
 fi
 
-for arg in "$@" ; do
-#echo "Evaluating parameter \"$arg\""
-    if [ "$arg" == "g" ]; then
-		echo "Setting" [$interface] "interface to Google DNS"
-        edit_nameserver_interface $gdns1 $gdns2
-		exit
-    fi
-
-    if [ "$arg" == "o" ]; then
-		echo "Setting" [$interface] "interface to OpenDNS"
-		edit_nameserver_interface $odns1 $odns2
-		exit
-    fi
-
-    if [ "$arg" == "a" ]; then
+while getopts :aghop option; do
+  case "${option}" in
+    a)
+        a=${OPTARG}
         echo "Setting" [$interface] "interface to DNS autoassign from DHCP"
-		edit_nameserver_interface empty
-		exit
-    fi
-
-    if [ "$arg" == "p" ]; then
+        edit_nameserver_interface empty
+        exit;;
+    g)
+        a=${OPTARG}
+        echo "Setting" [$interface] "interface to Google DNS"
+        edit_nameserver_interface $gdns1 $gdns2
+        exit;;
+    h)
+        h=${OPTARG}
+        print_usage
+        exit;;
+    o)
+        a=${OPTARG}
+        echo "Setting" [$interface] "interface to OpenDNS"
+        edit_nameserver_interface $odns1 $odns2
+        exit;;
+    p)
+        p=${OPTARG}
         print_dns_entry
-		exit
-    fi
-    
-    echo "Nothing can be done with parameter \"$arg\""
-	print_usage
-	exit
+        exit;;
+    *)
+        echo $prog: illegal option -- ${OPTARG}
+        print_usage
+        ;;
+  esac
 done
+
