d04cfb9494ea945ecb5e1a226231d676d64af25c
mike.holzinger <mike.holzinger@gmail.com>
2014-08-19 16:51:40 -0700

    Scrub Finder detritus from removable flash media

--- osx/scrub_flash_card.sh
+++ osx/scrub_flash_card.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# rm finder tags
 find . -name ".DS_Store" -print0 | xargs -0 rm
 find . -name "._*" -print0  | xargs -0 rm
 
-
+# rm empty folders
+find . -name ".Spotlight-V100" -print0  | xargs -0 rm -rf
+find . -name ".Trashes" -print0  | xargs -0 rm -rf
+find . -name ".fseventsd" -print0  | xargs -0 rm -rf
