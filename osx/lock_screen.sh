f1cb97c90530d4be721cbe107e9fd3dac15735ce
Mike Holzinger <mike.holzinger@gmail.com>
2015-05-15 19:08:19 -0700

    Lock Screen

---
+++ osx/lock_screen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Immediately locks screen session
+/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+
+# To add to alias commands: 
+# alias lockscreen='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
+
+
