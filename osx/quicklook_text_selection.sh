9d10032e11b97941fa3b296383c6b8747775baa1
Michael Holzinger <mike.holzinger@gmail.com>
2015-06-29 14:40:40 -0700

    Enable text selection in OS X Quick Look

---
+++ osx/quicklook_text_selection.sh
@@ -0,0 +1,9 @@
+# Set to False
+#defaults write com.apple.finder QLEnableTextSelection -bool FALSE
+#killall Finder
+
+
+# Set to true
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+killall Finder
+
