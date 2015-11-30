1d8acbe4eff3e649bf4d4cbad566fbd18193c855
Mike Holzinger <mike.holzinger@gmail.com>
2015-11-29 17:14:08 -0800

    Create new sftp locked-down sftp user on RHEL/CENTOS

--- new_locked_down_sftp.sh
+++ new_locked_down_sftp.sh
@@ -1,8 +1,13 @@
 # Use these commands to create a locked down sftp user
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 if [ -z "$1" ]
   then
-    echo "Supply a username for the ftp account as the parameter to this script"
+    echo "Supply a name for the new sftp user as the parameter to this script"
     exit
 fi
 
@@ -26,7 +31,17 @@ EOL
 
 NEW_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
-echo enter new password for $USERNAME : $NEW_PASS
+echo Password for $USERNAME : $NEW_PASS
 
-passwd "$USERNAME" --stdin
+echo "$NEW_PASS" | passwd "$USERNAME" --stdin
+
 /etc/init.d/sshd restart
+
+IPADDY=$(curl -s icanhazip.com -4)
+
+echo -------------------------
+echo Credentials
+echo Username: $USERNAME
+echo password: $NEW_PASS
+echo 
+echo sftp command: sftp $USERNAME@$IPADDY
