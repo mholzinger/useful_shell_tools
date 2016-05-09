ca20737a8f74178dead2abfdaa89b333bc818021
Michael Holzinger <mike.holzinger@gmail.com>
2016-05-08 19:05:27 -0700

    rsync commands

---
+++ rsync_command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Edit this script to meet your needs.
+# Always test first with '--dry-run' (remove --dry-run to run the commands) 
+
+REMOTE_IP=192.168.0.1
+SOURCE=/tmp/my_folder
+DESTINATION=/tmp/
+SSH_USER=admin
+
+
+# Copy a File from a Local Server to a Remote Server with SSH
+rsync -avzh $SOURCE $SSH_USER@$REMOTE_IP:$DESTINATION --dry-run
+
+# Copy a File from a Remote Server to a Local Server with SSH
+rsync -avzh $SSH_USER@$REMOTE_IP:$DESTINATION $SOURCE --dry-run
+
+# Copy/Sync a File on a Local Computer (creates directory is one isn't there)
+rsync -azvh $SOURCE $DESTINATION --dry-run
