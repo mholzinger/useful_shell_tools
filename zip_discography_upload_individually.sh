c654c9813b00cfaacadecbfd13465f8b40e42587
Mike Holzinger <mholzing@us.ibm.com>
2016-08-18 11:55:25 -0700

    Zip a discography in a directory by indivudual folder, upload to transfer.sh and clean up - leaving only a receipt

---
+++ zip_discography_upload_individually.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+receipt=transfer_list.txt
+
+# Upload to transfer.sh
+transfer()
+{
+    tmpfile=$( mktemp -t transferXXX ); 
+
+    if tty -s; then 
+        basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+        curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile
+    else 
+        curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ;
+    fi
+
+    cat $tmpfile
+    cat $tmpfile >> $receipt
+    rm -f $tmpfile
+}
+
+# Clean house first
+find . -name ".DS_Store" -print0 | xargs -0 rm
+find . -name "._*" -print0  | xargs -0 rm
+
+# Clean up transfer list
+if [ -f $receipt ]; then
+  :> $receipt
+  echo "List of zipped and transferred files" >> $receipt
+fi
+
+# Find directories with albums, then zip them
+for dir in */
+do
+    echo zipping $dir
+    output_name=$( echo "$dir" | sed -e 's/ /\_/g' -e 's/\//\.zip/g')
+
+    zip -r -X "$output_name"  "$dir"
+ 
+    # Upload to transfer.sh
+    if [ -f $output_name ]; then
+        transfer ./"$output_name"
+    fi
+
+    # Clean up
+    rm -f ./"$output_name"
+done
+
+echo
+cat $receipt
+
