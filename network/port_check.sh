9c58d239ce056844deb7a1df138e0f5412bdeaae
Mike Holzinger <mholzing@us.ibm.com>
2017-01-08 14:17:41 -0800

    Test for host listening at TCP/UDP port

---
+++ network/port_check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+port_to_test=80
+
+# Check for null string passed (or no string)
+if [ -z "$1" ]
+  then 
+    echo "Usage: port_check [ hostname / url / ip address ] [port]"
+    echo " -- default port is 80"
+  exit
+fi
+
+# Test for port being set 
+if [ ! -z "$2" ]; then
+	port_to_test=$2
+fi
+
+#Now let's use netcat : nc -zv google.com 22
+nc -zv $1 $port_to_test
+
