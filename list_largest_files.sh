b00aeaa22d1f57e5398bfaedcd2ebb3ca27ea19c
Mike Holzinger <mholzinger@us.ibm.com>
2017-05-25 17:24:19 -0700

    Better awk

--- list_largest_files.sh
+++ list_largest_files.sh
@@ -21,8 +21,8 @@ df -h "$FS";
 echo && echo "Largest Directories:"
 du -x "$FS" 2>/dev/null| sort -rnk1 |\
   head -n $NUMRESULTS |\
-  awk '{printf "%d MB - ", $1/1024; for (i=2; i<=NF; i++) printf "%s ",$i;printf "\n"}'
-
+  awk '{printf "%d MB - ", $1/1024; $1="";print}'
+  
 echo && echo "Largest Files:"
 nice -n 19 find "$FS" \
   -mount \
@@ -31,3 +31,4 @@ nice -n 19 find "$FS" \
   sort -rnk7 |\
   head -n $NUMRESULTS |\
   awk '{printf "%d MB - ", ($7/1024)/1024; for (i=11; i<=NF; i++) printf "%s ",$i;printf "\n"}'
+#  awk '{printf "%d MB - ", ($7/1024)/1024; $1="";$2="";$3="";$4="";$5="";$6="";$7="";$8="";$9="";$10="";print}'
