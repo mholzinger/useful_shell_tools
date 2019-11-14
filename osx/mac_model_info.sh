99d4a8d25dfcabe156479cb978bfddb43ce69867
Mike Holzinger <mike.holzinger@gmail.com>
2019-11-14 07:23:12 -0800

    Adds snippet to verify official Mac Model info

--- osx/mac_model_info.sh
+++ osx/mac_model_info.sh
@@ -1,8 +1,7 @@
+system_profiler SPHardwareDataType | grep "Model Identifier" |cut -d ':' -f 2
+
 curl -s https://support-sp.apple.com/sp/product?cc=$(
   system_profiler SPHardwareDataType \
     | awk '/Serial/ {print $4}' \
     | cut -c 9-
 ) | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|'
-
-system_profiler SPHardwareDataType | grep "Model Identifier"
-
