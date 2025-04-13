045f4f9738ca563f819858685e409da417fed42d
Mike Holzinger <mike.holzinger@gmail.com>
2025-04-13 17:24:57 -0400

    Removes icmp test and directly interfaces with system level tcp

--- network-io/amiup.sh
+++ network-io/amiup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+# This script checks internet connectivity using TCP instead of ICMP, 
+# as many service providers block ICMP (ping) packets. It attempts to 
+# connect to a well-known host (google.com) on port 80 (HTTP). If the 
+# connection succeeds, a success message is displayed. Otherwise, an 
+# error message is shown, and the script exits with a non-zero status.
+# Usage: ./amiup.sh
+
 ping_internets() {
-	ping -c 2 google.com > /dev/null 2>&1
+    host="google.com"
+    port=80
 
-	check_err "Nope - google.com is not reachable. No internets for you :["
-
-	# if we have a 0 exit status for ping we report this
-	echo "Yay! google.com is reachable. You're online!"
+    # Test connectivity using /dev/tcp
+    (echo >/dev/tcp/${host}/${port}) &>/dev/null && \
+	  echo "Yay! ${host} is reachable. You're online!" ||\
+	  check_err "Nope - ${host} is not reachable. No internets for you :["
 }
 
-check_err(){ error_state=$(echo $?)
-if [[ "$error_state" != "0" ]];then
-    echo $1
-    exit
-fi
+check_err() {
+    echo "$1"
+    exit 1
 }
 
 ping_internets
